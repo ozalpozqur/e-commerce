c7193e39b160d69abb98aef0ad53d0e20677cf12
Özgür ÖZALP <ozgur@altogic.com>
2022-12-23 12:25:00 +0300

    updated

--- src/pages/checkout/Success.tsx
+++ src/pages/checkout/Success.tsx
@@ -1,12 +1,3 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-
 export default function Success() {
-	const location = useLocation();
-
-	useEffect(() => {
-		console.log(location);
-	}, []);
-
 	return <></>;
 }
