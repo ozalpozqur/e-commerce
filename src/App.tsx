9abc4c8403198d2707507eabe3e86588b6002e6a
Özgür ÖZALP <ozgur@altogic.com>
2022-12-28 00:38:10 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import ScreenLoader from './components/ScreenLoader';
 
 export default function App() {
-	return <RouterProvider router={router} />;
+	return <RouterProvider fallbackElement={<ScreenLoader />} router={router} />;
 }
