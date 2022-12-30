c2ddc993549f3bdbc8baf1162d323beb721d7f05
Özgür ÖZALP <ozgur@altogic.com>
2022-12-30 16:53:27 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
 import ProductList from '../components/ProductList';
+import FeaturedProducts from '../components/FeaturedProducts';
 
 export default function Home() {
-	return <ProductList />;
+	return (
+		<section>
+			<FeaturedProducts />
+			<div className="text-center my-10 text-3xl md:text-4xl uppercase underline underline-offset-8">
+				All Products
+			</div>
+			<ProductList />
+		</section>
+	);
 }
