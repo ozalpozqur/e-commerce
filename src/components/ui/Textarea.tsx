99299f6d488bd3cff5bc1f6150bbbfb418f9fd11
Özgür ÖZALP <ozgur@altogic.com>
2023-01-03 14:17:19 +0300

    Update Textarea.tsx

--- src/components/ui/Textarea.tsx
+++ src/components/ui/Textarea.tsx
@@ -4,37 +4,29 @@ import { cn } from '../../helpers';
 export interface TextAreaProps extends ComponentPropsWithRef<'textarea'> {
 	showError?: boolean;
 	errorMessage?: string;
-	label?: string;
 	className?: string;
 }
 const TextArea = forwardRef(
 	(
-		{ className, rows, cols, label, showError = false, errorMessage, ...props }: TextAreaProps,
+		{ className, rows, cols, showError = false, errorMessage, ...props }: TextAreaProps,
 		ref: ForwardedRef<HTMLTextAreaElement>
 	) => {
 		const id = useId();
 
 		return (
 			<div className={className}>
-				{label ? (
-					<label htmlFor={id} className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-						{label}
-					</label>
-				) : undefined}
-				<div className="mt-1 sm:mt-0 sm:col-span-2">
-					<textarea
-						ref={ref}
-						id={id}
-						rows={rows}
-						cols={cols}
-						className={cn(
-							'shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md',
-							showError ? 'border-red-600' : ''
-						)}
-						{...props}
-					/>
-					{showError ? <small className="text-red-600">{errorMessage}</small> : undefined}
-				</div>
+				<textarea
+					ref={ref}
+					id={id}
+					rows={rows}
+					cols={cols}
+					className={cn(
+						'shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md',
+						showError ? 'border-red-600' : ''
+					)}
+					{...props}
+				/>
+				{showError ? <small className="text-red-600">{errorMessage}</small> : undefined}
 			</div>
 		);
 	}
