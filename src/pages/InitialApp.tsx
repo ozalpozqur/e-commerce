1c44181049b1df53b5137a8b58dd2bfb20efa11a
Özgür ÖZALP <ozgur@altogic.com>
2023-01-04 13:15:30 +0300

    Update InitialApp.tsx

--- src/pages/InitialApp.tsx
+++ src/pages/InitialApp.tsx
@@ -1,12 +1,21 @@
-import { Outlet, useLoaderData } from 'react-router-dom';
+import { Outlet, useLoaderData, useRevalidator } from 'react-router-dom';
 import { useEffect } from 'react';
 import altogic from '../libs/altogic';
-import { CartService } from '../services';
+import { CartService, OrderService } from '../services';
 import { RootLoader } from '../loaders';
-import { useColorStore, useCategoryStore, useAuthStore, useProductStore, useCartStore, useSizeStore } from '../store';
-import product from '../store/product';
+import {
+	useColorStore,
+	useCategoryStore,
+	useAuthStore,
+	useProductStore,
+	useCartStore,
+	useSizeStore,
+	useAdminStore
+} from '../store';
+import { toast } from 'react-toastify';
 
 export default function InitialApp() {
+	const { revalidate } = useRevalidator();
 	const { products, categories, cart, activeCategories, colors, sizes } = useLoaderData() as RootLoader;
 	const { setProducts } = useProductStore();
 	const { setCategories, setActiveCategories } = useCategoryStore();
@@ -14,6 +23,7 @@ export default function InitialApp() {
 	const { user } = useAuthStore();
 	const { setColors } = useColorStore();
 	const { setSizes } = useSizeStore();
+	const { setWaitingOrderCount } = useAdminStore();
 
 	useEffect(() => {
 		setProducts(products.items, products.paginateData);
@@ -25,13 +35,17 @@ export default function InitialApp() {
 
 		if (user) {
 			altogic.realtime.join(user?._id);
+			if (user.isAdmin) altogic.realtime.join('admin');
+
 			altogic.realtime.on('cleared-cart', clearCart);
 			altogic.realtime.on('fetch-cart', fetchCart);
+			altogic.realtime.on('waiting-order-count', fetchWaitingOrderCount);
 		}
 
 		return () => {
 			altogic.realtime.off('cleared-cart', clearCart);
 			altogic.realtime.off('fetch-cart', fetchCart);
+			altogic.realtime.off('waiting-order-count', fetchWaitingOrderCount);
 		};
 	}, []);
 
@@ -41,6 +55,12 @@ export default function InitialApp() {
 
 	async function fetchCart() {
 		setCart(await CartService.getCart());
+	}
+
+	async function fetchWaitingOrderCount({ message }: { message: boolean }) {
+		if (message) toast.info("You've got new order");
+		revalidate();
+		setWaitingOrderCount(await OrderService.getWaitingOrderCount());
 	}
 
 	return <Outlet />;
