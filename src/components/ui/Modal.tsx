d694729f5b9d2ca6554d6441267a23b081598094
Özgür ÖZALP <ozgur@altogic.com>
2023-01-06 16:34:54 +0300

    Update Modal.tsx

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -10,17 +10,14 @@ interface ModalProps {
 export default function Modal({ isOpen, close, children }: ModalProps) {
 	useEffect(() => {
 		if (!isOpen) return;
+
 		function handler(event: KeyboardEvent) {
-			if (event.key === 'Escape') {
-				close?.();
-			}
+			if (event.key === 'Escape') close?.();
 		}
 
 		document.addEventListener('keydown', handler);
 
-		return () => {
-			document.removeEventListener('keydown', handler);
-		};
+		return () => document.removeEventListener('keydown', handler);
 	}, [isOpen]);
 
 	function clickOutSide(event: MouseEvent) {
