b8e731b4ab1ac712ac2e67b9c81cbf0f42cd4661
Özgür ÖZALP <ozgur@altogic.com>
2023-01-06 13:00:46 +0300

    Update InputModal.tsx

--- src/components/InputModal.tsx
+++ src/components/InputModal.tsx
@@ -1,77 +1,65 @@
-import { Modal } from 'flowbite-react';
 import Button from './ui/Button';
-import { FormEvent, MouseEvent, useEffect, useId, useRef, useState } from 'react';
+import { FormEvent, useId, useRef } from 'react';
 import Input from './ui/Input';
-import { Tooltip } from 'react-tooltip';
+import Modal from './ui/Modal';
 
 interface InputModalProps {
 	isOpen: boolean;
 	label?: string;
 	close: () => void;
-	onSubmit: (value: string) => void;
+	onSubmit: (value: string, resetFunction: () => void) => void;
 	loading?: boolean;
 	defaultValue?: string;
+	inputPlaceholder?: string;
+	inputType?: 'text' | 'password' | 'email' | 'number' | 'tel' | 'url';
+	buttonText?: string;
 }
-export default function InputModal({ isOpen, defaultValue, close, onSubmit, loading, label }: InputModalProps) {
+
+export default function InputModal({
+	isOpen,
+	defaultValue,
+	inputPlaceholder,
+	inputType,
+	close,
+	onSubmit,
+	buttonText,
+	loading,
+	label
+}: InputModalProps) {
 	const input = useRef<HTMLInputElement>(null);
 	const id = useId();
 
-	useEffect(() => {
-		if (!isOpen) return;
-		function handler(event: KeyboardEvent) {
-			if (event.key === 'Escape') {
-				close?.();
-			}
-		}
-
-		document.addEventListener('keydown', handler);
-
-		return () => {
-			document.removeEventListener('keydown', handler);
-		};
-	}, [isOpen]);
-
-	function clickOutSide(event: MouseEvent) {
-		if (event.target === event.currentTarget) close?.();
-	}
-
 	function submitHandler(e: FormEvent) {
 		e.preventDefault();
-		if (input.current?.value) onSubmit(input.current.value);
+		if (input.current?.value) onSubmit(input.current.value, resetInput);
+	}
+
+	function resetInput() {
+		if (input.current) input.current.value = '';
 	}
 
 	return (
-		<Modal
-			onClick={clickOutSide}
-			className="!h-full [&>:first-child]:h-auto"
-			show={isOpen}
-			size="md"
-			popup={true}
-			onClose={() => close?.()}
-		>
-			<Modal.Header />
-			<Modal.Body>
-				<form onSubmit={submitHandler}>
-					<div className="flex flex-col gap-1 mb-2">
-						{label && (
-							<label className="font-medium text-sm" htmlFor={id}>
-								{label}
-							</label>
-						)}
-						<Input
-							defaultValue={defaultValue}
-							ref={input}
-							placeholder="Type order's tracking URL"
-							type="url"
-							id={id}
-							required
-						/>
-					</div>
-					<Button loading={loading} full type="submit">
-						Save
-					</Button>
-				</form>
-			</Modal.Body>
+		<Modal isOpen={isOpen} close={close}>
+			<form onSubmit={submitHandler}>
+				<div className="flex flex-col gap-1 mb-2">
+					{label && (
+						<label className="font-medium text-sm" htmlFor={id}>
+							{label}
+						</label>
+					)}
+					<Input
+						defaultValue={defaultValue}
+						ref={input}
+						placeholder={inputPlaceholder}
+						type={inputType}
+						id={id}
+						required
+					/>
+				</div>
+				<Button loading={loading} full type="submit">
+					{buttonText ?? 'Save'}
+				</Button>
+			</form>
 		</Modal>
 	);
 }
