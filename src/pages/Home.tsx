d12729b046fda17eac18e2ab83d266b4d85a411c
Özgür ÖZALP <ozgur@altogic.com>
2023-01-09 12:32:22 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,11 @@ import FeaturedProducts from '../components/FeaturedProducts';
 import { useProductStore } from '../store';
 
 export default function Home() {
-	const { paginateData } = useProductStore();
+	const { paginateData, products } = useProductStore();
 	return (
 		<section>
 			<FeaturedProducts />
-			{paginateData.currentPage === 1 && (
+			{paginateData.currentPage === 1 && products.length >= 3 && (
 				<div className="text-center my-10 text-3xl md:text-4xl uppercase underline underline-offset-8">
 					All Products
 				</div>
