119721c639b7bff069ccfef6fb41cd52cf4d9a69
Özgür ÖZALP <ozgur@altogic.com>
2023-01-09 13:17:48 +0300

    Update Orders.tsx

--- src/pages/admin/Orders.tsx
+++ src/pages/admin/Orders.tsx
@@ -14,7 +14,6 @@ import { APIError } from 'altogic';
 import altogic from '../../libs/altogic';
 import InputModal from '../../components/InputModal';
 import { FaShippingFast } from 'react-icons/all';
-import { Tooltip } from 'react-tooltip';
 import SearchOrder from '../../components/SearchOrder';
 const status: OrderStatus[] = ['waiting', 'preparing', 'shipped', 'completed', 'canceled'];
 
